81e04425e4560468916f8aacaec40203e7b1aeab
Matt Logan <matt.b.logan@gmail.com>
2014-11-29 14:11:17 -0800

    Added ArtisteTest

---
+++ library/src/androidTest/java/me/mattlogan/artiste/ArtisteTest.java
@@ -0,0 +1,57 @@
+package me.mattlogan.artiste;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class ArtisteTest extends MockitoUnitTestCase {
+
+    @Mock Shape shape;
+    @Mock Paint paint;
+    @Mock Canvas canvas;
+    Rect rect; // final classes can't be mocked
+
+    Artiste artiste;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        rect = new Rect();
+        artiste = new Artiste();
+    }
+
+    public void testDrawShape() {
+        artiste.drawShape(shape);
+        assertEquals(shape, artiste.shape);
+    }
+
+    public void testWithPaint() {
+        artiste.withPaint(paint);
+        assertEquals(paint, artiste.paint);
+    }
+
+    public void testOnCanvas() {
+        artiste.shape = shape;
+        artiste.paint = paint;
+        artiste.shape.setBounds(rect);
+
+        artiste.onCanvas(canvas);
+
+        verify(canvas, times(1)).drawPath(shape.getPath(), paint);
+    }
+
+    public void testClearAll() {
+        artiste.shape = shape;
+        artiste.paint = paint;
+
+        artiste.clearAll();
+
+        assertNull(artiste.shape);
+        assertNull(artiste.paint);
+    }
+}
