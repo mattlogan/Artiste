8a0493b2d031e63cc4a0ef79a2dab9a687360297
mattlogan <matt.b.logan@gmail.com>
2015-02-22 14:18:40 -0800

    Big refactor -- Artiste now stateless class with static methods

--- example/src/main/java/me/mattlogan/artiste/example/AssortedShapeView.java
+++ example/src/main/java/me/mattlogan/artiste/example/AssortedShapeView.java
@@ -3,51 +3,44 @@ package me.mattlogan.artiste.example;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
-import me.mattlogan.artiste.Shapes;
+import me.mattlogan.artiste.Artiste;
 
 public class AssortedShapeView extends View {
 
-    Shapes.Hexagon hexagon;
-    Shapes.FivePointedStar fivePointedStar;
-    Decagram decagram;
-    Shapes.Circle circle;
-    Octagram octagram;
-    Shapes.Pentagon pentagon;
+    private Path hexagonPath;
+    private Path fivePointedStarPath;
+    private Path decagramPath;
+    private Path circlePath;
+    private Path octagramPath;
+    private Path pentagonPath;
 
-    Paint hexagonPaint;
-    Paint fivePointedStarPaint;
-    Paint decagramPaint;
-    Paint circlePaint;
-    Paint octagramPaint;
-    Paint pentagonPaint;
+    private Paint hexagonPaint;
+    private Paint fivePointedStarPaint;
+    private Paint decagramPaint;
+    private Paint circlePaint;
+    private Paint octagramPaint;
+    private Paint pentagonPaint;
 
     public AssortedShapeView(Context context) {
         super(context);
-        init();
+        initPaint();
     }
 
     public AssortedShapeView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        initPaint();
     }
 
     public AssortedShapeView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        initPaint();
     }
 
-    void init() {
-        hexagon = new Shapes.Hexagon();
-        fivePointedStar = new Shapes.FivePointedStar();
-        decagram = new Decagram();
-        circle = new Shapes.Circle();
-        octagram = new Octagram();
-        pentagon = new Shapes.Pentagon();
-
+    private void initPaint() {
         hexagonPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         hexagonPaint.setColor(0xFF0263ED);
         hexagonPaint.setStrokeWidth(3);
@@ -79,22 +72,22 @@ public class AssortedShapeView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        hexagon.calculatePath(new Rect(0, 0, h, h), 25);
-        fivePointedStar.calculatePath(new Rect(h, 0, 2 * h, h), 12);
-        decagram.calculatePath(new Rect(2 * h, 0, 3 * h, h), 18);
-        circle.calculatePath(new Rect(3 * h, 0, 4 * h, h), 0);
-        octagram.calculatePath(new Rect(4 * h, 0, 5 * h, h), 22.5f);
-        pentagon.calculatePath(new Rect(5 * h, 0, 6 * h, h), 50);
+        hexagonPath = Artiste.createRegularConvexPolygon(0, 0, h, h, 6, 25);
+        fivePointedStarPath = Artiste.createRegularStarPolygon(h, 0, 2 * h, h, 5, 2, 12);
+        decagramPath = Artiste.createRegularStarPolygon(2 * h, 0, 3 * h, h, 10, 3, 18, false);
+        circlePath = Artiste.createCircle(3 * h, 0, 4 * h, h);
+        octagramPath = Artiste.createRegularStarPolygon(4 * h, 0, 5 * h, h, 8, 3, 22.5f, false);
+        pentagonPath = Artiste.createRegularConvexPolygon(5 * h, 0, 6 * h, h, 5, 50);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPath(hexagon.getPath(), hexagonPaint);
-        canvas.drawPath(fivePointedStar.getPath(), fivePointedStarPaint);
-        canvas.drawPath(decagram.getPath(), decagramPaint);
-        canvas.drawPath(circle.getPath(), circlePaint);
-        canvas.drawPath(octagram.getPath(), octagramPaint);
-        canvas.drawPath(pentagon.getPath(), pentagonPaint);
+        canvas.drawPath(hexagonPath, hexagonPaint);
+        canvas.drawPath(fivePointedStarPath, fivePointedStarPaint);
+        canvas.drawPath(decagramPath, decagramPaint);
+        canvas.drawPath(circlePath, circlePaint);
+        canvas.drawPath(octagramPath, octagramPaint);
+        canvas.drawPath(pentagonPath, pentagonPaint);
     }
 }
--- library/src/main/java/me/mattlogan/artiste/Shape.java
+++
@@ -1,9 +0,0 @@
-package me.mattlogan.artiste;
-
-import android.graphics.Path;
-import android.graphics.Rect;
-
-public interface Shape {
-    public abstract void calculatePath(Rect rect, float rotationDegrees);  // calculation heavy, should be called outside of onDraw()
-    public abstract Path getPath();
-}
