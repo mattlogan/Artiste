0333cc5e9d76ccc9d071c5d7b7e1b54546c5d6b3
Matt Logan <matt.b.logan@gmail.com>
2015-09-15 09:42:16 -0600

    Put "use once" MathUtils methods back into Paths.regularStarPolygon()

--- library/src/main/java/me/mattlogan/artiste/MathUtils.java
+++ library/src/main/java/me/mattlogan/artiste/MathUtils.java
@@ -1,14 +1,7 @@
 package me.mattlogan.artiste;
 
-import android.graphics.Path;
-
-import static java.lang.Math.cos;
-import static java.lang.Math.max;
-import static java.lang.Math.min;
 import static java.lang.Math.pow;
-import static java.lang.Math.sin;
 import static java.lang.Math.sqrt;
-import static java.lang.Math.toRadians;
 
 final class MathUtils {
 
@@ -26,121 +19,5 @@ final class MathUtils {
 
     static float yIntercept(float[] point, float slope) {
         return point[1] - slope * point[0];
-    }
-
-    static float[][] makeOuterPointsArray(int numPoints, int density, float startDegrees,
-                                          float r) {
-
-        float degreesBetweenPoints = 360f / numPoints;
-        float[][] outerPoints = new float[numPoints][2];
-
-        for (int i = 0; i < numPoints; i++) {
-            double theta = toRadians(startDegrees + density * i * degreesBetweenPoints);
-            outerPoints[i][0] = (float) (r + (r * cos(theta)));
-            outerPoints[i][1] = (float) (r - (r * sin(theta)));
-        }
-        return outerPoints;
-    }
-
-    static float[] findFirstIntersectionPoint(float[][] pointsArray) {
-        float[] firstPt1 = pointsArray[0];
-        float[] firstPt2 = pointsArray[1];
-
-        float firstSlope = slope(firstPt1, firstPt2);
-        float firstYInt = yIntercept(firstPt1, firstSlope);
-
-        // Ranges for first line. We'll use these later to check if the intersection we find
-        // is in the valid range.
-        float firstLowX = min(firstPt1[0], firstPt2[0]);
-        float firstHighX = max(firstPt1[0], firstPt2[0]);
-        float firstLowY = min(firstPt1[0], firstPt2[1]);
-        float firstHighY = max(firstPt1[1], firstPt2[1]);
-
-        // The second line and the last line can't intersect the first line. Skip them.
-        for (int i = 2; i < pointsArray.length - 1; i++) {
-            float[] curPt1 = pointsArray[i];
-            float[] curPt2 = pointsArray[i + 1];
-
-            float curSlope = slope(curPt1, curPt2);
-            float curYInt = curPt1[1] - curSlope * curPt1[0];
-
-            // System of equations. Two equations, two unknowns.
-            // y = firstSlope * x + firstYInt
-            // y = curSlope * x + curYInt
-
-            // Solve for x and y in terms of known quantities.
-            // firstSlope * x + firstYInt = curSlope * x + curYInt
-            // firstSlope * x - curSlope * x = curYInt - firstYInt
-            // x * (firstSlope - curSlope) = (curYInt - firstYInt)
-            // x = (curYInt - firstYInt) / (firstSlope - curSlope)
-            // y = firstSlope * x + firstYInt
-
-            if (firstSlope == curSlope) {
-                // lines can't intersect if they are parallel
-                continue;
-            }
-
-            float intersectionX = (curYInt - firstYInt) / (firstSlope - curSlope);
-            float intersectionY = firstSlope * intersectionX + firstYInt;
-
-            // Ranges for current line.
-            float curLowX = min(curPt1[0], curPt2[0]);
-            float curHighX = max(curPt1[0], curPt2[0]);
-            float curLowY = min(curPt1[0], curPt2[1]);
-            float curHighY = max(curPt1[1], curPt2[1]);
-
-            // Range where intersection has to be.
-            float startX = max(firstLowX, curLowX);
-            float endX = min(firstHighX, curHighX);
-            float startY = max(firstLowY, curLowY);
-            float endY = min(firstHighY, curHighY);
-
-            if (intersectionX > startX && intersectionX < endX &&
-                    intersectionY > startY && intersectionY < endY) {
-                // Found intersection.
-                return new float[]{intersectionX, intersectionY};
-            }
-        }
-
-        // If there are no intersections, it's not a star polygon.
-        throw new IllegalStateException("Failed to calculate path." +
-                "Are the number of points and density valid?");
-    }
-
-    static float[][] makeOutlinePointsArray(int numPoints, float startDegrees,
-                                            float outerRadius, float innerRadius) {
-
-        float degreesBetweenPoints = 360f / numPoints;
-        float[][] outlinePoints = new float[numPoints][2];
-
-        for (int i = 0; i < numPoints; i++) {
-            double theta = toRadians(startDegrees + i * degreesBetweenPoints);
-
-            float radius = i % 2 == 0 ? outerRadius : innerRadius;
-
-            outlinePoints[i][0] = (float) (outerRadius + (radius * cos(theta)));
-            outlinePoints[i][1] = (float) (outerRadius - (radius * sin(theta)));
-        }
-
-        return outlinePoints;
-    }
-
-    static Path createStarPathFromPointsArray(float xOffset, float yOffset,
-                                              float[][] pointsArray) {
-
-        Path path = new Path();
-        for (int i = 0; i < pointsArray.length; i++) {
-            float x = xOffset + pointsArray[i][0];
-            float y = yOffset + pointsArray[i][1];
-
-            if (i == 0) {
-                path.moveTo(x, y);
-            } else {
-                path.lineTo(x, y);
-            }
-        }
-        path.lineTo(xOffset + pointsArray[0][0], yOffset + pointsArray[0][1]);
-
-        return path;
     }
 }
--- library/src/main/java/me/mattlogan/artiste/Paths.java
+++ library/src/main/java/me/mattlogan/artiste/Paths.java
@@ -4,6 +4,8 @@ import android.graphics.Path;
 import android.graphics.RectF;
 
 import static java.lang.Math.cos;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 import static java.lang.Math.sin;
 import static java.lang.Math.toRadians;
 import static me.mattlogan.artiste.MathUtils.*;
@@ -95,29 +97,129 @@ public final class Paths {
             throw new IllegalArgumentException("Density must be at least 2");
         }
 
-        float radius = (right - left) / 2f;
+        float outerRadius = (right - left) / 2f;
 
         // Add 90 so first point is top
         float startDegrees = 90 + rotationDegrees;
 
-        float[][] outerPointsArray = makeOuterPointsArray(numPoints, density, startDegrees, radius);
+        float degreesBetweenPoints = 360f / numPoints;
+
+        float[][] outerPointsArray = new float[numPoints][2];
+        for (int i = 0; i < numPoints; i++) {
+            double theta = toRadians(startDegrees + density * i * degreesBetweenPoints);
+            outerPointsArray[i][0] = (float) (outerRadius + (outerRadius * cos(theta)));
+            outerPointsArray[i][1] = (float) (outerRadius - (outerRadius * sin(theta)));
+        }
+
+        float[][] pointsForPath;
 
         if (outline) {
             // Find the first intersection point created by drawing each line in the star
-            float[] firstIntersection = findFirstIntersectionPoint(outerPointsArray);
+            float[] firstIntersection = null;
+
+            float[] firstPt1 = outerPointsArray[0];
+            float[] firstPt2 = outerPointsArray[1];
+
+            float firstSlope = slope(firstPt1, firstPt2);
+            float firstYInt = yIntercept(firstPt1, firstSlope);
+
+            // Ranges for first line. We'll use these later to check if the intersection we find
+            // is in the valid range.
+            float firstLowX = min(firstPt1[0], firstPt2[0]);
+            float firstHighX = max(firstPt1[0], firstPt2[0]);
+            float firstLowY = min(firstPt1[0], firstPt2[1]);
+            float firstHighY = max(firstPt1[1], firstPt2[1]);
+
+            // The second line and the last line can't intersect the first line. Skip them.
+            for (int i = 2; i < outerPointsArray.length - 1; i++) {
+                float[] curPt1 = outerPointsArray[i];
+                float[] curPt2 = outerPointsArray[i + 1];
+
+                float curSlope = slope(curPt1, curPt2);
+                float curYInt = curPt1[1] - curSlope * curPt1[0];
+
+                // System of equations. Two equations, two unknowns.
+                // y = firstSlope * x + firstYInt
+                // y = curSlope * x + curYInt
+
+                // Solve for x and y in terms of known quantities.
+                // firstSlope * x + firstYInt = curSlope * x + curYInt
+                // firstSlope * x - curSlope * x = curYInt - firstYInt
+                // x * (firstSlope - curSlope) = (curYInt - firstYInt)
+                // x = (curYInt - firstYInt) / (firstSlope - curSlope)
+                // y = firstSlope * x + firstYInt
+
+                if (firstSlope == curSlope) {
+                    // lines can't intersect if they are parallel
+                    continue;
+                }
+
+                float intersectionX = (curYInt - firstYInt) / (firstSlope - curSlope);
+                float intersectionY = firstSlope * intersectionX + firstYInt;
+
+                // Ranges for current line.
+                float curLowX = min(curPt1[0], curPt2[0]);
+                float curHighX = max(curPt1[0], curPt2[0]);
+                float curLowY = min(curPt1[0], curPt2[1]);
+                float curHighY = max(curPt1[1], curPt2[1]);
+
+                // Range where intersection has to be.
+                float startX = max(firstLowX, curLowX);
+                float endX = min(firstHighX, curHighX);
+                float startY = max(firstLowY, curLowY);
+                float endY = min(firstHighY, curHighY);
+
+                if (intersectionX > startX && intersectionX < endX &&
+                        intersectionY > startY && intersectionY < endY) {
+                    // Found intersection.
+                    firstIntersection = new float[]{intersectionX, intersectionY};
+                }
+            }
+
+            if (firstIntersection == null) {
+                // If there are no intersections, it's not a star polygon.
+                throw new IllegalStateException("Failed to calculate path." +
+                        "Are the number of points and density valid?");
+            }
 
             // Use the first intersection point to find the radius of the inner circle of the star
-            float innerRadius = distance(radius, radius, firstIntersection[0],
+            float innerRadius = distance(outerRadius, outerRadius, firstIntersection[0],
                     firstIntersection[1]);
 
-            // Make the array of each point in the star outline
-            float[][] outlinePointsArray = makeOutlinePointsArray(numPoints * 2, startDegrees,
-                    radius, innerRadius);
+            // There are now twice as many points to "line to" in our path
+            numPoints *= 2;
 
-            return createStarPathFromPointsArray(left, top, outlinePointsArray);
+            // Recalculate degrees between points
+            degreesBetweenPoints = 360f / numPoints;
+
+            pointsForPath = new float[numPoints][2];
+
+            for (int i = 0; i < numPoints; i++) {
+                double theta = toRadians(startDegrees + i * degreesBetweenPoints);
+                float radius = i % 2 == 0 ? outerRadius : innerRadius;
+
+                pointsForPath[i][0] = (float) (outerRadius + (radius * cos(theta)));
+                pointsForPath[i][1] = (float) (outerRadius - (radius * sin(theta)));
+            }
         } else {
-            return createStarPathFromPointsArray(left, top, outerPointsArray);
+            pointsForPath = outerPointsArray;
         }
+
+        // Make the Path from whatever points array we're using -- outline or not
+        Path path = new Path();
+        for (int i = 0; i < pointsForPath.length; i++) {
+            float x = left + pointsForPath[i][0];
+            float y = top + pointsForPath[i][1];
+
+            if (i == 0) {
+                path.moveTo(x, y);
+            } else {
+                path.lineTo(x, y);
+            }
+        }
+        path.lineTo(left + pointsForPath[0][0], top + pointsForPath[0][1]);
+
+        return path;
     }
 
     /**
